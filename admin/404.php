2b6dfb04c324c7b04c2811b783cf8ba652a0e336
JoeTheBest <jewelshahi10@gmail.com>
2024-01-08 22:28:45 +0200

    added

---
+++ admin/404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Page Not Found - 404</title>
+  <link rel="stylesheet" href="../css/404.css">
+</head>
+
+<body>
+  <img src="../images/404.svg" alt="404 page not found">
+  <h2>
+    Oops! Page Not Found.
+  </h2>
+  <h5>
+    The page you are looking for doesn't exist or another error has occurred.
+  </h5>
+  <a href="dashboard.php">Go Back</a>
+</body>
+
+</html>
