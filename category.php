618cd361ad795ed555fa0b861eae37f3f927626b
JoeTheBest <jewelshahi10@gmail.com>
2024-01-09 17:57:06 +0200

    version 2

--- category.php
+++ category.php
@@ -27,6 +27,8 @@ include 'components/wishlist_cart.php';
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
   <!-- custom css file link  -->
+  <link rel="stylesheet" href="css/global.css">
+
   <link rel="stylesheet" href="css/style.css">
 
 </head>
--- checkout.php
+++ checkout.php
@@ -57,6 +57,8 @@ if (isset($_POST['order'])) {
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
    <!-- custom css file link  -->
+  <link rel="stylesheet" href="css/global.css">
+
    <link rel="stylesheet" href="css/style.css">
 </head>
 
