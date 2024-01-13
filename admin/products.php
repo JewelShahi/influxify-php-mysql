60e508d5cebf5c1636d6cc0657440c9f0218fad3
JoeTheBest <jewelshahi10@gmail.com>
2024-01-13 20:22:12 +0200

    everything is ok

--- admin/products.php
+++ admin/products.php
@@ -23,19 +23,19 @@ if (isset($_POST['add_product'])) {
   $image_01 = filter_var($image_01, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $image_size_01 = $_FILES['image_01']['size'];
   $image_tmp_name_01 = $_FILES['image_01']['tmp_name'];
-  $image_folder_01 = '../uploaded_img/' . $image_01;
+  $image_folder_01 = '../uploaded_img/products/' . $image_01;
 
   $image_02 = $_FILES['image_02']['name'];
   $image_02 = filter_var($image_02, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $image_size_02 = $_FILES['image_02']['size'];
   $image_tmp_name_02 = $_FILES['image_02']['tmp_name'];
-  $image_folder_02 = '../uploaded_img/' . $image_02;
+  $image_folder_02 = '../uploaded_img/products/' . $image_02;
 
   $image_03 = $_FILES['image_03']['name'];
   $image_03 = filter_var($image_03, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $image_size_03 = $_FILES['image_03']['size'];
   $image_tmp_name_03 = $_FILES['image_03']['tmp_name'];
-  $image_folder_03 = '../uploaded_img/' . $image_03;
+  $image_folder_03 = '../uploaded_img/products/' . $image_03;
 
   $select_products = $conn->prepare("SELECT * FROM `products` WHERE name = ?");
   $select_products->execute([$name]);
@@ -140,7 +140,7 @@ if (isset($_GET['delete'])) {
         while ($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)) {
       ?>
           <div class="box">
-            <img src="../uploaded_img/<?= $fetch_products['image_01']; ?>" alt="">
+            <img src="../uploaded_img/products/<?= $fetch_products['image_01']; ?>" alt="">
             <div class="name"><?= $fetch_products['name']; ?></div>
             <div class="price">$<span><?= $fetch_products['price']; ?></span>/-</div>
             <div class="details"><span><?= $fetch_products['details']; ?></span></div>
--- admin/update_product.php
+++ admin/update_product.php
@@ -30,7 +30,7 @@ if (isset($_POST['update'])) {
   $image_01 = filter_var($image_01, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $image_size_01 = $_FILES['image_01']['size'];
   $image_tmp_name_01 = $_FILES['image_01']['tmp_name'];
-  $image_folder_01 = '../uploaded_img/' . $image_01;
+  $image_folder_01 = '../uploaded_img/products/' . $image_01;
 
   if (!empty($image_01)) {
     if ($image_size_01 > 2000000) {
@@ -39,7 +39,7 @@ if (isset($_POST['update'])) {
       $update_image_01 = $conn->prepare("UPDATE `products` SET image_01 = ? WHERE id = ?");
       $update_image_01->execute([$image_01, $pid]);
       move_uploaded_file($image_tmp_name_01, $image_folder_01);
-      unlink('../uploaded_img/' . $old_image_01);
+      unlink('../uploaded_img/products/' . $old_image_01);
       $message[] = 'Image-01 has been updated successfully!';
     }
   }
@@ -49,7 +49,7 @@ if (isset($_POST['update'])) {
   $image_02 = filter_var($image_02, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $image_size_02 = $_FILES['image_02']['size'];
   $image_tmp_name_02 = $_FILES['image_02']['tmp_name'];
-  $image_folder_02 = '../uploaded_img/' . $image_02;
+  $image_folder_02 = '../uploaded_img/products/' . $image_02;
 
   if (!empty($image_02)) {
     if ($image_size_02 > 2000000) {
@@ -58,7 +58,7 @@ if (isset($_POST['update'])) {
       $update_image_02 = $conn->prepare("UPDATE `products` SET image_02 = ? WHERE id = ?");
       $update_image_02->execute([$image_02, $pid]);
       move_uploaded_file($image_tmp_name_02, $image_folder_02);
-      unlink('../uploaded_img/' . $old_image_02);
+      unlink('../uploaded_img/products/' . $old_image_02);
       $message[] = 'Image-02 has been updated successfully!';
     }
   }
@@ -68,7 +68,7 @@ if (isset($_POST['update'])) {
   $image_03 = filter_var($image_03, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
   $image_size_03 = $_FILES['image_03']['size'];
   $image_tmp_name_03 = $_FILES['image_03']['tmp_name'];
-  $image_folder_03 = '../uploaded_img/' . $image_03;
+  $image_folder_03 = '../uploaded_img/products/' . $image_03;
 
   if (!empty($image_03)) {
     if ($image_size_03 > 2000000) {
@@ -77,7 +77,7 @@ if (isset($_POST['update'])) {
       $update_image_03 = $conn->prepare("UPDATE `products` SET image_03 = ? WHERE id = ?");
       $update_image_03->execute([$image_03, $pid]);
       move_uploaded_file($image_tmp_name_03, $image_folder_03);
-      unlink('../uploaded_img/' . $old_image_03);
+      unlink('../uploaded_img/products/' . $old_image_03);
       $message[] = 'Image-03 has been updated successfully!';
     }
   }
@@ -118,12 +118,12 @@ if (isset($_POST['update'])) {
           <input type="hidden" name="old_image_03" value="<?= $fetch_products['image_03']; ?>">
           <div class="image-container">
             <div class="main-image">
-              <img src="../uploaded_img/<?= $fetch_products['image_01']; ?>" alt="">
+              <img src="../uploaded_img/products/<?= $fetch_products['image_01']; ?>" alt="">
             </div>
             <div class="sub-image">
-              <img src="../uploaded_img/<?= $fetch_products['image_01']; ?>" alt="">
-              <img src="../uploaded_img/<?= $fetch_products['image_02']; ?>" alt="">
-              <img src="../uploaded_img/<?= $fetch_products['image_03']; ?>" alt="">
+              <img src="../uploaded_img/products/<?= $fetch_products['image_01']; ?>" alt="">
+              <img src="../uploaded_img/products/<?= $fetch_products['image_02']; ?>" alt="">
+              <img src="../uploaded_img/products/<?= $fetch_products['image_03']; ?>" alt="">
             </div>
           </div>
           <span>Update Name</span>
