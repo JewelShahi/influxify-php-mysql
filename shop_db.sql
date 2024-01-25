5b356c82edb811737a137b6fc739cb24caf485f9
JoeTheBest <jewelshahi10@gmail.com>
2024-01-25 20:55:36 +0200

    version 16 db chnaged (name)

--- shop_db.sql
+++ shop_db.sql
@@ -15,6 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE shop_db;
+
+USE shop_db;
+
 --
 -- Table structure for table `cart`
 --
