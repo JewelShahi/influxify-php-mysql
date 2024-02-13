ee67ead900ae8be30a7da465066caa340d0798e3
JoeTheBest <jewelshahi10@gmail.com>
2024-02-13 17:12:27 +0200

    order updated, service updated, new added

--- components/connect.php
+++ components/connect.php
@@ -4,6 +4,10 @@ $db_name = 'mysql:host=localhost;dbname=shop_db';
 $user_name = 'root';
 $user_password = '';
 
-$conn = new PDO($db_name, $user_name, $user_password);
+try {
+  $conn = new PDO($db_name, $user_name, $user_password);
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 
 ?>
