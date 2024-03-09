9bf28d6883cc670ca2be4c1cff1af5d6acc84037
JoeTheBest <jewelshahi10@gmail.com>
2024-03-09 09:43:01 +0200

    testamonial updated and fixed

--- user/testamonial.php
+++ user/testamonial.php
@@ -1,107 +1,106 @@
 <div class="main">
-		<div class="testimonials">
-			<input type="radio" name="testimonial" id="input-testimonial1" checked />
-			<input type="radio" name="testimonial" id="input-testimonial2" />
-			<input type="radio" name="testimonial" id="input-testimonial3" />
-			<input type="radio" name="testimonial" id="input-testimonial4" />
-			<div class="testimonials-inner">
-				<div class="testimonial">
-					<div class="testimonial-photo">
-						<div class="photo-background"></div>
-						<div class="photo-author"></div>
-					</div>
-					<div class="testimonial-text">
-						<p>
-							"Outstanding service and a diverse range of top-quality phones
-							make this shop the ultimate destination for all your mobile
-							needs."
-						</p>
-					</div>
-					<div class="testimonial-author">Ichigo Kurosaki</div>
+	<div class="testimonials">
+		<input type="radio" name="testimonial" id="input-testimonial1" checked />
+		<input type="radio" name="testimonial" id="input-testimonial2" />
+		<input type="radio" name="testimonial" id="input-testimonial3" />
+		<input type="radio" name="testimonial" id="input-testimonial4" />
+		<div class="testimonials-inner">
+			<div class="testimonial">
+				<div class="testimonial-photo">
+					<div class="photo-background"></div>
+					<div class="photo-author"></div>
 				</div>
-				<div class="testimonial">
-					<div class="testimonial-photo">
-						<div class="photo-background"></div>
-						<div class="photo-author"></div>
-					</div>
-					<div class="testimonial-text">
-						<p>
-							"Discover unparalleled expertise and the latest in mobile
-							technology at our trusted phone shop."
-						</p>
-					</div>
-					<div class="testimonial-author">Satorou Gojo</div>
+				<div class="testimonial-text">
+					<p>
+						"Outstanding service and a diverse range of top-quality phones
+						make this shop the ultimate destination for all your mobile
+						needs."
+					</p>
 				</div>
-				<div class="testimonial">
-					<div class="testimonial-photo">
-						<div class="photo-background"></div>
-						<div class="photo-author"></div>
-					</div>
-					<div class="testimonial-text">
-						<p>
-							"At our phone shop, expect nothing less than personalized attention and top-tier products tailored to your mobile lifestyle."
-						</p>
-					</div>
-					<div class="testimonial-author">Wang Ling</div>
-				</div>
-				<div class="testimonial">
-					<div class="testimonial-photo">
-						<div class="photo-background"></div>
-						<div class="photo-author"></div>
-					</div>
-					<div class="testimonial-text">
-						<p>
-							"Experience seamless service and find your perfect phone match at our reliable phone shop."
-						</p>
-					</div>
-					<div class="testimonial-author">Sung Jinwoo</div>
-				</div>
+				<div class="testimonial-author">Ichigo Kurosaki</div>
 			</div>
-			<div class="testimonials-arrows">
-				<div class="arrow arrow-left">
-					<label for="input-testimonial1"></label>
-					<label for="input-testimonial2"></label>
-					<label for="input-testimonial3"></label>
-					<label for="input-testimonial4"></label>
-					<span></span>
+			<div class="testimonial">
+				<div class="testimonial-photo">
+					<div class="photo-background"></div>
+					<div class="photo-author"></div>
 				</div>
-				<div class="arrow arrow-right">
-					<label for="input-testimonial1"></label>
-					<label for="input-testimonial2"></label>
-					<label for="input-testimonial3"></label>
-					<label for="input-testimonial4"></label>
-					<span></span>
+				<div class="testimonial-text">
+					<p>
+						"At Influxity, discover the latest smartphones that seamlessly blend innovation with style, elevating your tech experience to new heights."
+					</p>
 				</div>
+				<div class="testimonial-author">Satorou Gojo</div>
 			</div>
-			<div class="testimonials-bullets">
-				<label for="input-testimonial1">
-					<div class="bullet">
-						<div>
-							<span></span>
-						</div>
-					</div>
-				</label>
-				<label for="input-testimonial2">
-					<div class="bullet">
-						<div>
-							<span></span>
-						</div>
-					</div>
-				</label>
-				<label for="input-testimonial3">
-					<div class="bullet">
-						<div>
-							<span></span>
-						</div>
-					</div>
-				</label>
-				<label for="input-testimonial4">
-					<div class="bullet">
-						<div>
-							<span></span>
-						</div>
-					</div>
-				</label>
+			<div class="testimonial">
+				<div class="testimonial-photo">
+					<div class="photo-background"></div>
+					<div class="photo-author"></div>
+				</div>
+				<div class="testimonial-text">
+					<p>
+						"Influxity transformed my phone shopping experience into a journey of excitement and discovery, where every device felt like a window to a world of endless possibilities."
+					</p>
+				</div>
+				<div class="testimonial-author">Wang Ling</div>
+			</div>
+			<div class="testimonial">
+				<div class="testimonial-photo">
+					<div class="photo-background"></div>
+					<div class="photo-author"></div>
+				</div>
+				<div class="testimonial-text">
+					<p>
+						"Experience seamless service and find your perfect phone match at our reliable phone shop."
+					</p>
+				</div>
+				<div class="testimonial-author">Sung Jinwoo</div>
 			</div>
 		</div>
+		<div class="testimonials-arrows">
+			<div class="arrow arrow-left">
+				<label for="input-testimonial1"></label>
+				<label for="input-testimonial2"></label>
+				<label for="input-testimonial3"></label>
+				<label for="input-testimonial4"></label>
+				<span></span>
+			</div>
+			<div class="arrow arrow-right">
+				<label for="input-testimonial1"></label>
+				<label for="input-testimonial2"></label>
+				<label for="input-testimonial3"></label>
+				<label for="input-testimonial4"></label>
+				<span></span>
+			</div>
+		</div>
+		<div class="testimonials-bullets">
+			<label for="input-testimonial1">
+				<div class="bullet">
+					<div>
+						<span></span>
+					</div>
+				</div>
+			</label>
+			<label for="input-testimonial2">
+				<div class="bullet">
+					<div>
+						<span></span>
+					</div>
+				</div>
+			</label>
+			<label for="input-testimonial3">
+				<div class="bullet">
+					<div>
+						<span></span>
+					</div>
+				</div>
+			</label>
+			<label for="input-testimonial4">
+				<div class="bullet">
+					<div>
+						<span></span>
+					</div>
+				</div>
+			</label>
+		</div>
 	</div>
+</div>
