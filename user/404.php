0f4915ddc16ffb4b09211d0a59ddc814a9d9f036
JoeTheBest <jewelshahi10@gmail.com>
2024-03-09 12:48:30 +0200

    testamonial updated and fixed

--- user/404.php
+++ user/404.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Page Not Found - 404</title>
-  <link rel="shortcut icon" href="images/influxify-logo.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="../images/influxify-logo.ico" type="image/x-icon">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
   <link rel="stylesheet" href="../css/404.css">
   <link rel="stylesheet" href="../css/global.css">
